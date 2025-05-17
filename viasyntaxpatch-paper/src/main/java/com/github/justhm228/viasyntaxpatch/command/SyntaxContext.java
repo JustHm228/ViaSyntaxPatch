39e35168c48936385cd427768d25718f761193fc
JustHuman228 <justhuman228.github@gmail.com>
2025-05-17 11:01:24 +0300

    Create ``SyntaxContext.java``
    Create a ``SyntaxContext`` class which will hold
    some parameters required for patching the syntax.

---
+++ viasyntaxpatch-paper/src/main/java/com/github/justhm228/viasyntaxpatch/command/SyntaxContext.java
@@ -0,0 +1,78 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2025 Chirkunov Egor
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.justhm228.viasyntaxpatch.command;
+
+import com.github.justhm228.viasyntaxpatch.ViaSyntaxPatch;
+import com.mojang.brigadier.tree.CommandNode;
+import com.mojang.brigadier.tree.RootCommandNode;
+import org.bukkit.entity.Player;
+
+public abstract class SyntaxContext<S> {
+
+	private final ViaSyntaxPatch plugin;
+
+	private final Player player;
+
+	private final RootCommandNode<S> rootNode;
+
+	private final CommandNode<S> headNode;
+
+	private final CommandNode<S> parentNode;
+
+	protected SyntaxContext(final ViaSyntaxPatch plugin, final Player player, final RootCommandNode<S> rootNode, final CommandNode<S> headNode, final CommandNode<S> parentNode) {
+
+		super();
+		this.plugin = plugin;
+		this.player = player;
+		this.rootNode = rootNode;
+		this.headNode = headNode;
+		this.parentNode = parentNode;
+	}
+
+	public final ViaSyntaxPatch getPlugin() {
+
+		return plugin;
+	}
+
+	public final Player getPlayer() {
+
+		return player;
+	}
+
+	public final RootCommandNode<S> getRootNode() {
+
+		return rootNode;
+	}
+
+	public final CommandNode<S> getHeadNode() {
+
+		return headNode;
+	}
+
+	public final CommandNode<S> getParentNode() {
+
+		return parentNode;
+	}
+}
